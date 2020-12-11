cbe5e09935946272ee3a7e1525695da4eb79173d
鱼鱼 <59079424+yuyu888@users.noreply.github.com>
2020-12-11 17:43:34 +0800

    Update DataMultiplex.php

--- DataMultiplex.php
+++ DataMultiplex.php
@@ -1,19 +1,26 @@
+<?php
 class  DataMultiplex
 {
- 
- public function get_is_attribute($x_rank, $attribute)
- {
-  if ($x_rank & $attribute) return true; else return false;
-  
- }
- public function set_add_attribute($x_rank, $attribute)
- {
-  return ($x_rank | $attribute);
- }
 
- public function set_del_attribute($x_rank, $attribute)
- {
-  $_temp = ~$attribute;
-  return ($x_rank & $_temp);
- }
+    public function get_is_attribute($x_rank, $attribute)
+    {
+        if ($attribute>$x_rank) return false;
+        if ($x_rank & $attribute) return true; else return false;
+
+    }
+    public function set_add_attribute($x_rank, $attribute)
+    {
+        return ($x_rank | $attribute);
+    }
+
+    public function set_del_attribute($x_rank, $attribute)
+    {
+        $_temp = ~$attribute;
+        return ($x_rank & $_temp);
+    }
+
+    public function check_attribute($attribute){
+        return ($attribute&($attribute-1))===0;
+    }
+
 }
