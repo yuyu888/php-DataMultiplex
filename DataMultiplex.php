291f35ca8609cee74ee873290729287a2844e9f5
鱼鱼 <59079424+yuyu888@users.noreply.github.com>
2021-01-06 10:14:07 +0800

    Update DataMultiplex.php

--- DataMultiplex.php
+++ DataMultiplex.php
@@ -25,16 +25,11 @@ class  DataMultiplex
     // 属性遍历
     public function get_attribute_list($x_rank){
         $list = array();
-		$n=1;
-        while($x_rank>0){
-            // &表示按位与，0x开头的表示的是十六进制数，k&0x1表示k与0x1按位与，其效果为取k的二进制中最右边的数字
-			if($x_rank&0x1){ 
-				$list[] = $n;
-			}
-			$n = $n<<1;
-            $x_rank = $x_rank>>1;
-        }
-        return $list;
+	while($x_rank != 0){
+	  $list[] = $x_rank-($x_rank & ($x_rank-1));
+	  $x_rank = $x_rank & ($x_rank-1);
+	}
+	return $list;
     }
 
 }
