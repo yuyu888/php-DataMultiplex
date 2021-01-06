d1983c6336fc9df03b2e9abe8ba33a1b7448a61e
鱼鱼 <59079424+yuyu888@users.noreply.github.com>
2021-01-06 10:10:20 +0800

    Update DataMultiplex.php

--- DataMultiplex.php
+++ DataMultiplex.php
@@ -4,9 +4,7 @@ class  DataMultiplex
 
     public function get_is_attribute($x_rank, $attribute)
     {
-        if ($attribute>$x_rank) return false;
-        if ($x_rank & $attribute) return true; else return false;
-
+        return ($x_rank&$attribute)==$attribute;
     }
     public function set_add_attribute($x_rank, $attribute)
     {
@@ -19,8 +17,24 @@ class  DataMultiplex
         return ($x_rank & $_temp);
     }
 
+    // 检查是否是2的n次方
     public function check_attribute($attribute){
-        return ($attribute&($attribute-1))===0;
+        return !($attribute&($attribute-1));
+    }
+
+    // 属性遍历
+    public function get_attribute_list($x_rank){
+        $list = array();
+		$n=1;
+        while($x_rank>0){
+            // &表示按位与，0x开头的表示的是十六进制数，k&0x1表示k与0x1按位与，其效果为取k的二进制中最右边的数字
+			if($x_rank&0x1){ 
+				$list[] = $n;
+			}
+			$n = $n<<1;
+            $x_rank = $x_rank>>1;
+        }
+        return $list;
     }
 
 }
